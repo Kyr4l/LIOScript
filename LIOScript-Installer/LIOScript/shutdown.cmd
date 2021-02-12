5ee3ca6f82f4368e043afc17f46e813643f71e46
Kyr4l <alanemmanuel5@gmail.com>
2021-02-12 16:57:27 +0100

    cleanup

--- LIOScript-Installer/LIOScript/shutdown.cmd
+++ LIOScript-Installer/LIOScript/shutdown.cmd
@@ -2,17 +2,7 @@
 
 echo Purge de la session
 del /S /Q "%USERPROFILE%\Desktop\*"
-del /S /Q "%USERPROFILE%\Pictures\*"
-del /S /Q "%USERPROFILE%\Saved Games\*"
-del /S /Q "%USERPROFILE%\3D Objects\*"
-del /S /Q "%USERPROFILE%\Music\*"
-del /S /Q "%USERPROFILE%\Documents\*"
 del /S /Q "%USERPROFILE%\Downloads\*"
-del /S /Q "%USERPROFILE%\Contacts\*"
-del /S /Q "%USERPROFILE%\Favorites\*"
-del /S /Q "%USERPROFILE%\Videos\*"
-del /S /Q "%USERPROFILE%\Links\*"
-del /S /Q "%USERPROFILE%\Searches\*"
 
 echo Extinction de l'ordinateur
 shutdown /s /f /t 1
