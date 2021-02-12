a9505e4d923095870178231475234d67a05bf0ea
Kyr4l <alanemmanuel5@gmail.com>
2021-02-12 16:49:50 +0100

    hotfix

--- LIOScript-Installer/LIOScript/open-session.cmd
+++ LIOScript-Installer/LIOScript/open-session.cmd
@@ -1,7 +1,10 @@
 @echo off
 
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -incognito
+
 copy /Y /V "%USERPROFILE%\AppData\Local\LIOScript\shortcuts\D‚connexion.lnk" "%USERPROFILE%\Desktop"
 copy /Y /V "%USERPROFILE%\AppData\Local\LIOScript\shortcuts\Eteindre.lnk" "%USERPROFILE%\Desktop"
+copy /Y /V "%USERPROFILE%\AppData\Local\LIOScript\shortcuts\Chrome.lnk" "%USERPROFILE%\Desktop"
 
 cscript loginmsg.vbs "Avertissement"
 
